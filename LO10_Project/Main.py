04bdff6fd535c449bfc587df3de1d87f137e883a
Rinkichi75 <cocmithulan@gmail.com>
2022-06-12 01:27:51 +0200

    FIX/CORS: Implement flask-cors

--- LO10_Project/Main.py
+++ LO10_Project/Main.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 import ChampionTrendsAnalyzer
 import Champion
 import Summoner
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/")
 def hello_world():
