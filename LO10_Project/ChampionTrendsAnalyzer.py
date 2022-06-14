0c5a9e75a747ceec9504e09c55ace5248c4ed083
RaphaelBarcelo <69222248+RaphaelBarcelo@users.noreply.github.com>
2022-06-14 11:35:51 +0200

    Updated algorithm to handle exceptions

--- LO10_Project/ChampionTrendsAnalyzer.py
+++ LO10_Project/ChampionTrendsAnalyzer.py
@@ -8,17 +8,21 @@ from pytrends.request import TrendReq
 import random
 import Connection
 
-champions = {}
 path = "C://Users//zenit//Desktop//LO10 Project//data/"
+champions = {}
+problematic_champions = {}
 
 
 def init_champions():
+    print('Initiating champions')
     versions = Connection.watcher.data_dragon.versions_for_region(Connection.region_v4)
     champions_version = versions['n']['champion']
     current_champ_list = Connection.watcher.data_dragon.champions(champions_version)['data']
 
     for champion in current_champ_list:
         champions[champion] = current_champ_list[champion]['name']
+        problematic_champions[champion] = 0
+    problematic_champions['DrMundo'] = 4
 
 
 def create_csv_file():
@@ -27,27 +31,35 @@ def create_csv_file():
             pass
 
 
-# https://github.com/alexwlchan/handling-http-429-with-tenacity
 def initiate_csv_file():
-    init_champions()
     pytrends1 = TrendReq()
-    settings = initialize_VPN(area_input=['complete rotation'])
-    for champion_1, champion_1_name in champions.items():
-        if os.stat(path + champion_1 + '.csv').st_size == 0:
-            champion1_popularity_comparison = {}
-            for champion_2, champion_2_name in champions.items():
-                if champion_1 != champion_2:
-                    search_terms = [champion_1_name + ' league of legends', champion_2_name + ' league of legends']
-                    pytrends1.build_payload(search_terms, geo='US', timeframe="today 12-m")
-                    df1 = pytrends1.interest_over_time()
-                    champion1_popularity_comparison[champion_2] = df1[search_terms[0]].mean().round(0)
-                    print(champion_1 + ' : ' + champion_2 + ' (' +
-                          str(champion1_popularity_comparison[champion_2]) + ')')
-                    time.sleep(1.1)
-            with open(path + champion_1 + '.csv', 'w') as f:
-                w = csv.writer(f)
-                w.writerows(champion1_popularity_comparison.items())
-            rotate_VPN(settings)
+    #settings = initialize_VPN(area_input=['complete rotation'])
+    while True:
+        try:
+            for champion_1, champion_1_name in champions.items():
+                if os.stat(path + champion_1 + '.csv').st_size == 0 and problematic_champions[champion_1] < 3:
+                    champion1_popularity_comparison = {}
+                    # rotate_VPN(settings)
+                    problematic_champions[champion_1] += 1
+                    for champion_2, champion_2_name in champions.items():
+                        if champion_1 != champion_2:
+                            search_terms = [champion_1_name + ' league of legends',
+                                            champion_2_name + ' league of legends']
+                            pytrends1.build_payload(search_terms, geo='US', timeframe="today 12-m")
+                            df1 = pytrends1.interest_over_time()
+                            champion1_popularity_comparison[champion_2] = df1[search_terms[0]].mean().round(0)
+                            print(champion_1 + ' : ' + champion_2 + ' (' +
+                                  str(champion1_popularity_comparison[champion_2]) + ')')
+                            time.sleep(1.1)
+                    with open(path + champion_1 + '.csv', 'w') as f:
+                        pass
+                        #w = csv.writer(f)
+                        #w.writerows(champion1_popularity_comparison.items())
+            break
+        except Exception as e:
+            print(e)
+            time.sleep(1)
+            continue
 
 
 def get_ranking():
@@ -72,5 +84,6 @@ def get_ranking_random():
         champion_ranking[name] = random.randint(0, 100)
     return champion_ranking
 
-#init_champions()
-#initiate_csv_file()
+
+init_champions()
+initiate_csv_file()
